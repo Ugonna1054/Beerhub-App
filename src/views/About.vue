991595862c4068f66180a227a386221b25882b62
Ubaka Ugonna <40705982+Ugonna1054@users.noreply.github.com>
2019-07-31 21:44:57 +0100

    test

---
+++ src/views/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+    <mdb-container>
+      <mdb-row>
+        <mdb-col class="col-md-6">
+          xdjcjj
+        </mdb-col>
+       <mdb-badge>read</mdb-badge>
+         <mdb-col color=primary-color >
+           jdjdjdj
+        </mdb-col>
+      </mdb-row>
+    </mdb-container>
+  </div>
+</template>
+
+<script>
+import {mdbContainer, mdbRow, mdbCol, mdbBadge} from 'mdbvue';
+export default {
+  name:'about',
+  components:{
+    mdbContainer,
+    mdbRow,
+    mdbCol,
+    mdbBadge
+  }
+}
+</script>
